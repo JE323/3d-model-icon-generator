5b2d72cdb9ee499e273e74af8ffe973c443afb1c
JE <jonathaneyre323@gmail.com>
2018-07-15 17:19:47 +0100

    Updated to be FactoryStartup and parsing of argments
    Scripts changed to be run from factory startup and as such deleted the .blend files.
    
    Parsing of argments is now fixed with issues being caused by incorrect splitting of inputted strings to vector settings.

--- basicRenderTest.bat
+++ basicRenderTest.bat
@@ -1,5 +1,5 @@
-cd D:\\Progmramming\\IconGenerator
+cd G:\\"Blender Projects"\\blender-icon-generator
 
-Start C:\\"Program Files"\\blender-2.79a-rc-windows64\\blender.exe -b basicScene.blend -o //Output//file_ --render-format PNG -f 1
+Start C:\\"Program Files"\\"Blender Foundation"\\Blender\\blender.exe -b --factory-startup -o //Output// -P G:\\"Blender Projects"\\blender-icon-generator\\IconGenerator.py --render-format JPEG -f 1 -- -file G://"Blender Projects"//blender-icon-generator//ExampleObjects//monkey.obj -camType PERSP -renderSize "(1024,1024)" -pixBorder "10" -camRot "(60,0,30)" -samples "10" -bgColour "(1,1,1,1)" -vignette 0 >> G:\\"Blender Projects"\\blender-icon-generator\\Logs\\log.txt 2>&1
 
-PAUSE
+::PAUSE
