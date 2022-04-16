fa9e1a8569c6decc1f6de929a4a952a201f2e03f
JE <33268253+JE323@users.noreply.github.com>
2022-04-16 12:11:31 +0100

    Finalised spinup testing

--- service/psql/initTables.sql
+++ service/psql/initTables.sql
@@ -2,3 +2,8 @@ CREATE TABLE IF NOT EXISTS test (
     id text NOT NULL,
     title varchar(200) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS testSecond (
+    id text NOT NULL,
+    title varchar(200) NOT NULL
+);
