1e07898565fe92fc16084f1dc0ccedbabdac68ce
JE <33268253+JE323@users.noreply.github.com>
2022-04-16 13:00:03 +0100

    Created basic tables and data

--- service/psql/initTables.sql
+++ service/psql/initTables.sql
@@ -1,9 +1,25 @@
-CREATE TABLE IF NOT EXISTS test (
-    id text NOT NULL,
-    title varchar(200) NOT NULL
+create table if not exists status
+(
+    id   uuid not null,
+    name text
 );
 
-CREATE TABLE IF NOT EXISTS testSecond (
-    id text NOT NULL,
-    title varchar(200) NOT NULL
+create table if not exists jobs
+(
+    id        uuid not null,
+    name      text not null,
+    "modelId" uuid,
+    status    uuid,
+    constraint jobs_pk
+        primary key (id),
+    constraint status_fk
+        foreign key (id) references status (id)
 );
+
+create unique index if not exists jobs_id_uindex
+    on jobs (id);
+
+create unique index if not exists status_id_uindex
+    on status (id);
+
+
