c5dafcc44885c19e6451022fb01b14805a5ebfd2
JE <33268253+JE323@users.noreply.github.com>
2022-04-17 08:05:26 +0100

    Implemented Ping

---
+++ service/aspnetcore/src/thumbnailService/Controllers/MiscApi.Impl.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using thumbnailService.Models;
+
+namespace thumbnailService.Controllers;
+
+/// <inheritdoc />
+public class MiscApiControllerImpl : MiscApiController
+{
+    /// <inheritdoc />
+    public override IActionResult GetPing() => new OkObjectResult(new InlineResponse200());
+}
