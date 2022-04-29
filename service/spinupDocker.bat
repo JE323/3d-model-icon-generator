cacfc8a01bcb883827f9c27bc43519ea4c8cb00d
JE <33268253+JE323@users.noreply.github.com>
2022-04-29 22:48:00 +0100

    Upgrade to aspnetcore 6

--- service/spinupDocker.bat
+++ service/spinupDocker.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Rebuild the service image from the latest source
-docker build -t thumbnail_service aspnetcore
+docker build -t thumbnail_service thumbnailService
 
 docker-compose up -d
 
--- service/updateFromSpec.bat
+++ service/updateFromSpec.bat
@@ -2,6 +2,6 @@
 
 docker run --rm -v %CD%:/local openapitools/openapi-generator-cli:v6.0.0-beta validate -i /local/api.yaml --recommend
 
-docker run --rm -v %CD%:/local -v %CD%/aspnetcore:/aspnetcore openapitools/openapi-generator-cli:v6.0.0-beta generate -g aspnetcore -i /local/api.yaml --package-name thumbnailService -o aspnetcore -c /local/generatorConfig.json
+docker run --rm -v %CD%:/local -v %CD%/thumbnailService:/thumbnailService openapitools/openapi-generator-cli:v6.0.0-beta generate -g aspnetcore -i /local/api.yaml --package-name thumbnailService -o thumbnailService -c /local/generatorConfig.json
 
 pause
